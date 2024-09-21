bf28f764da7952c9709fa11e7c8da782052c751c
Colum Ferry <cferry09@gmail.com>
2024-09-21 18:14:35 +0100

    feat: initial scaffold

---
+++ packages/build/src/lib/build.spec.ts
@@ -0,0 +1,7 @@
+import { build } from './build';
+
+describe('build', () => {
+  it('should work', () => {
+    expect(build()).toEqual('build');
+  });
+});
---
+++ packages/build/vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite';
+
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
+
+export default defineConfig({
+  root: __dirname,
+  cacheDir: '../../node_modules/.vite/packages/build',
+
+  plugins: [nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
+
+  // Uncomment this if you are using workers.
+  // worker: {
+  //  plugins: [ nxViteTsPaths() ],
+  // },
+
+  test: {
+    watch: false,
+    globals: true,
+    environment: 'node',
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    reporters: ['default'],
+    coverage: {
+      reportsDirectory: '../../coverage/packages/build',
+      provider: 'v8',
+    },
+  },
+});
