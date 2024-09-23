0308d32302d209a078e47e1e36f48c2049329b67
Colum Ferry <cferry09@gmail.com>
2024-09-23 19:14:02 +0100

    fix(nx): add dependency on rspack

--- packages/nx-plugin/eslint.config.js
+++ packages/nx-plugin/eslint.config.js
@@ -9,6 +9,7 @@ module.exports = [
         'error',
         {
           ignoredFiles: ['{projectRoot}/eslint.config.{js,cjs,mjs}'],
+          ignoredDependencies: ['@ng-rspack/build'],
         },
       ],
     },
