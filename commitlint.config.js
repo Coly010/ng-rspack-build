79cf804c192cba84fb41b02ca65a71eac9c5cabf
Colum Ferry <cferry09@gmail.com>
2024-09-24 12:07:59 +0100

    chore(repo): add prepush verifcation (#2)

---
+++ commitlint.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'scope-enum': () => [
+      2,
+      'always',
+      ['repo', 'testing', 'ci', 'build', 'nx', 'nx-e2e'],
+    ],
+  },
+};
