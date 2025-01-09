ed760a9cfd89b7aaa4998c2c3c144c4216786ce8
Colum Ferry <cferry09@gmail.com>
2025-01-09 15:41:54 +0000

    fix(docs): plugin

--- apps/docs/plugins/plugin-netlify-adapter/index.js
+++ apps/docs/plugins/plugin-netlify-adapter/index.js
@@ -1,17 +1,19 @@
-import { dirname, resolve, join } from 'path/posix';
-import { cpSync } from 'fs';
+const { dirname, resolve, join } = require('path/posix');
+const { cpSync } = require('fs');
 
-export const onPostBuild = function ({ constants }) {
-  console.log('onPostBuild - moving browser assets');
-  const browserDistFolder = resolve(constants.PUBLISH_DIR, 'browser');
-  const destination = join(dirname(constants.FUNCTIONS_DIST), 'browser');
-  console.log(
-    'onPostBuild - browserDist, destination',
-    borwserDistFolder,
-    destination
-  );
-  cpSync(browserDistFolder, destination, {
-    recursive: true,
-  });
-  console.log('onPostBuild - finished moving browser assets');
+module.exports = {
+  onPostBuild: ({ constants }) => {
+    console.log('onPostBuild - moving browser assets');
+    const browserDistFolder = resolve(constants.PUBLISH_DIR, 'browser');
+    const destination = join(dirname(constants.FUNCTIONS_DIST), 'browser');
+    console.log(
+      'onPostBuild - browserDist, destination',
+      borwserDistFolder,
+      destination
+    );
+    cpSync(browserDistFolder, destination, {
+      recursive: true,
+    });
+    console.log('onPostBuild - finished moving browser assets');
+  },
 };
