9d3b87951cd1ee1f4f8461ae1a4bfeafe92fe76e
Michael <michael.hladky@push-based.io>
2025-01-15 04:10:26 +0100

    test(rsbuild-plugin-angular): test setup compiler function

--- packages/rsbuild-plugin-angular/src/lib/plugin/compilation/setup-compilation.ts
+++ packages/rsbuild-plugin-angular/src/lib/plugin/compilation/setup-compilation.ts
@@ -1,5 +1,5 @@
 import { RsbuildConfig } from '@rsbuild/core';
-import * as compilerCli from '@angular/compiler-cli';
+import { readConfiguration } from '@angular/compiler-cli';
 import * as ts from 'typescript';
 import { compileString as sassCompileString } from 'sass-embedded';
 import { augmentHostWithResources } from './augments';
@@ -7,41 +7,49 @@ import { PluginAngularOptions } from '../../models/plugin-options';
 import { ParallelCompilation } from '@angular/build/src/tools/angular/compilation/parallel-compilation';
 import { transformFileSync } from '@swc/core';
 
+export const DEFAULT_NG_COMPILER_OPTIONS: ts.CompilerOptions = {
+  suppressOutputPathCheck: true,
+  outDir: undefined,
+  sourceMap: false,
+  inlineSourceMap: true,
+  inlineSources: true,
+  declaration: false,
+  declarationMap: false,
+  allowEmptyCodegenFiles: false,
+  annotationsAs: 'decorators',
+  enableResourceInlining: false,
+  noEmitOnError: false,
+  mapRoot: undefined,
+  sourceRoot: undefined,
+  supportTestBed: false,
+  supportJitMode: false,
+};
+
 export function setupCompilation(
-  config: RsbuildConfig,
-  options: PluginAngularOptions,
+  config: Pick<RsbuildConfig, 'mode' | 'source'>,
+  options: Pick<
+    PluginAngularOptions,
+    'tsconfigPath' | 'jit' | 'inlineStylesExtension'
+  >,
+  // @TODO isServer is only used if useAllRoots is false, so the logical order of the parameter should be changed
   isServer = false,
   useAllRoots = false
 ) {
   const isProd = config.mode === 'production';
 
-  const { options: tsCompilerOptions, rootNames } =
-    compilerCli.readConfiguration(
-      config.source?.tsconfigPath ?? options.tsconfigPath,
-      {
-        suppressOutputPathCheck: true,
-        outDir: undefined,
-        sourceMap: false,
-        declaration: false,
-        declarationMap: false,
-        allowEmptyCodegenFiles: false,
-        annotationsAs: 'decorators',
-        enableResourceInlining: false,
-        noEmitOnError: false,
-        mapRoot: undefined,
-        sourceRoot: undefined,
-        supportTestBed: false,
-        supportJitMode: false,
-      }
-    );
+  const parsedConfig = readConfiguration(
+    config.source?.tsconfigPath ?? options.tsconfigPath,
+    {
+      ...DEFAULT_NG_COMPILER_OPTIONS,
+      inlineSourceMap: !isProd,
+      inlineSources: !isProd,
+    }
+  );
+
+  const { options: tsCompilerOptions, rootNames } = parsedConfig;
 
   const compilerOptions = tsCompilerOptions;
   const host = ts.createIncrementalCompilerHost(compilerOptions);
-
-  const styleTransform = (code: string) => {
-    const result = sassCompileString(code);
-    return result.css;
-  };
 
   if (!options.jit) {
     augmentHostWithResources(host, styleTransform, {
@@ -50,17 +58,21 @@ export function setupCompilation(
     });
   }
 
-  const rn = useAllRoots
+  const filteredRootNames = useAllRoots
     ? rootNames
     : rootNames.filter((n) =>
         isServer ? !n.endsWith('main.ts') : n.endsWith('main.ts')
       );
 
   return {
-    rootNames: rn,
+    rootNames: filteredRootNames,
     compilerOptions,
     host,
   };
+}
+
+export function styleTransform(code: string) {
+  return sassCompileString(code).css;
 }
 
 export async function setupCompilationWithParallelCompilation(
