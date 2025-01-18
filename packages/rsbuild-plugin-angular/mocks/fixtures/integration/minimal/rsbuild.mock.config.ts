8e3c003268a70a0253994a428311cb34e41c9cf8
Michael <michael.hladky@push-based.io>
2025-01-18 16:10:01 +0100

    test: add fixtures

--- packages/rsbuild-plugin-angular/mocks/fixtures/integration/minimal/rsbuild.mock.config.ts
+++ packages/rsbuild-plugin-angular/mocks/fixtures/integration/minimal/rsbuild.mock.config.ts
@@ -3,5 +3,5 @@ import { createConfig } from '../../../../src';
 export default createConfig({
   browser: './src/main.ts',
   inlineStylesExtension: 'css',
-  tsconfigPath: './mocks/fixtures/integration/minimal/tsconfig.basic.json',
+  tsconfigPath: './mocks/fixtures/integration/minimal/tsconfig.mock.json',
 });
