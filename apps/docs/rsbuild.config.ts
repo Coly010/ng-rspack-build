95f103aa01103409dd8ce2c36da7b6c5290e47fd
Colum Ferry <cferry09@gmail.com>
2025-01-21 12:02:22 +0000

    chore(repo): fixup configs and snapshot

--- apps/docs/rsbuild.config.ts
+++ apps/docs/rsbuild.config.ts
@@ -1,4 +1,3 @@
-import { withConfigurations } from '@ng-rsbuild/plugin-angular';
 import { pluginSass } from '@rsbuild/plugin-sass';
 
 const options = {
@@ -9,23 +8,30 @@ const options = {
   styles: ['./src/styles.scss', './src/hljs.theme.scss'],
 };
 
-export default withConfigurations(
-  {
-    options,
-    rsbuildConfigOverrides: {
-      plugins: [pluginSass()],
-    },
-  },
-  {
-    production: {
-      options: {
-        fileReplacements: [
-          {
-            replace: './src/environments/environment.ts',
-            with: './src/environments/environment.prod.ts',
-          },
-        ],
+export default () => {
+  console.log('global.NX_GRAPH_CREATION', global.NX_GRAPH_CREATION);
+  if (global.NX_GRAPH_CREATION === undefined) {
+    const { withConfigurations } = require('@ng-rsbuild/plugin-angular');
+    return withConfigurations(
+      {
+        options,
+        rsbuildConfigOverrides: {
+          plugins: [pluginSass()],
+        },
       },
-    },
+      {
+        production: {
+          options: {
+            fileReplacements: [
+              {
+                replace: './src/environments/environment.ts',
+                with: './src/environments/environment.prod.ts',
+              },
+            ],
+          },
+        },
+      }
+    );
   }
-);
+  return {};
+};
