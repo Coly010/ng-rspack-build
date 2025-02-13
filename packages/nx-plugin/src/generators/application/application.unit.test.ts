89f96358aff9bf8f032029e75a8c53577629589c
Michael <michael.hladky@push-based.io>
2025-02-13 19:19:23 +0100

    test(nx): refactor expect case

--- packages/nx-plugin/src/generators/application/application.unit.test.ts
+++ packages/nx-plugin/src/generators/application/application.unit.test.ts
@@ -23,65 +23,51 @@ describe('Application Generator', () => {
 
     // ASSERT
     const project = readProjectConfiguration(tree, 'demo');
-    expect(project.targets?.build).toMatchInlineSnapshot(`
-      {
-        "configurations": {
-          "development": {
-            "mode": "development",
-          },
-          "production": {
-            "mode": "production",
-          },
+    expect(project.targets?.build).toStrictEqual({
+      configurations: {
+        development: {
+          mode: 'development',
         },
-        "defaultConfiguration": "production",
-        "executor": "@ng-rspack/nx:build",
-        "options": {
-          "assets": [
-            "./public",
-          ],
-          "index": "./src/index.html",
-          "main": "./src/main.ts",
-          "outputPath": "../../dist/apps/demo",
-          "polyfills": [
-            "zone.js",
-          ],
-          "styles": [
-            "./src/styles.css",
-          ],
-          "tsConfig": "./tsconfig.app.json",
+        production: {
+          mode: 'production',
         },
-        "outputs": [
-          "{options.outputPath}",
-        ],
-      }
-    `);
-    expect(project.targets?.serve).toMatchInlineSnapshot(`
-      {
-        "configurations": {
-          "development": {
-            "buildTarget": "demo:build:development",
-          },
-          "production": {
-            "buildTarget": "demo:build:production",
-          },
+      },
+      defaultConfiguration: 'production',
+      executor: '@ng-rspack/nx:build',
+      options: {
+        assets: ['./public'],
+        index: './src/index.html',
+        main: './src/main.ts',
+        outputPath: '../../dist/apps/demo',
+        polyfills: ['zone.js'],
+        styles: ['./src/styles.css'],
+        tsConfig: './tsconfig.app.json',
+      },
+      outputs: ['{options.outputPath}'],
+    });
+    expect(project.targets?.serve).toStrictEqual({
+      configurations: {
+        development: {
+          buildTarget: 'demo:build:development',
         },
-        "defaultConfiguration": "development",
-        "executor": "@ng-rspack/nx:serve",
-        "options": {
-          "port": 4200,
+        production: {
+          buildTarget: 'demo:build:production',
         },
-      }
-    `);
-    expect(project.targets?.['serve-static']).toMatchInlineSnapshot(`
-      {
-        "executor": "@nx/web:file-server",
-        "options": {
-          "buildTarget": "demo:build",
-          "port": 4200,
-          "spa": true,
-          "staticFilePath": "dist/apps/demo",
-        },
-      }
-    `);
+      },
+      defaultConfiguration: 'development',
+      executor: '@ng-rspack/nx:serve',
+      options: {
+        port: 4200,
+      },
+    });
+    expect(project.targets?.['serve-static']).toStrictEqual({
+      executor: '@nx/web:file-server',
+      options: {
+        buildTarget: 'demo:build',
+        port: 4200,
+        spa: true,
+        staticFilePath: 'dist/apps/demo',
+      },
+    });
   });
 });
