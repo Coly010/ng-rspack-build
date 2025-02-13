d34aba02de0d78fa847af4b69f8a14357812674f
Michael <michael.hladky@push-based.io>
2025-02-13 17:55:46 +0100

    fix: revert changes

---
+++ packages/nx-plugin/src/executors/serve/executor.spec.ts
@@ -0,0 +1,17 @@
+// import { ExecutorContext } from '@nx/devkit';
+//
+// import { BuildExecutorSchema } from './schema';
+// import executor from './executor';
+
+// const options: BuildExecutorSchema = {};
+// const context: ExecutorContext = {
+//   root: '',
+//   cwd: process.cwd(),
+//   isVerbose: false,
+// };
+
+describe('Serve Executor', () => {
+  it('can run', async () => {
+    expect(true).toBe(true);
+  });
+});
