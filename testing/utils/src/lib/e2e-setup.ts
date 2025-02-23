56898eb8c65e3bb24422456569048ade07524c28
Michael <michael.hladky@push-based.io>
2025-02-23 22:58:54 +0100

    ci: fix build

--- testing/utils/src/lib/e2e-setup.ts
+++ testing/utils/src/lib/e2e-setup.ts
@@ -1,6 +1,5 @@
-import * as console from 'node:console';
-import { join } from 'node:path';
-import { cp, readFile, rm, stat, writeFile } from 'node:fs/promises';
+import path from 'node:path';
+import fs from 'node:fs/promises';
 
 export function getE2eAppProjectName(): string | undefined {
   const e2eProjectName = process.env['NX_TASK_TARGET_PROJECT'] ?? '';
@@ -16,13 +15,13 @@ export async function setupE2eApp(
 ) {
   const targetProjectName =
     getE2eAppProjectName() ?? fixtureProjectName + '-e2e-app';
-  const fixture = join(
+  const fixture = path.join(
     __dirname,
     `../../../e2e/fixtures/${fixtureProjectName}`
   );
-  const target = join(__dirname, `../../../e2e/__test__/${targetProjectName}`);
+  const target = path.join(__dirname, `../../../e2e/__test__/${targetProjectName}`);
   try {
-    await stat(fixture);
+    await fs.stat(fixture);
   } catch (e) {
     console.warn(
       `Fixture folder not found. Did you change the file or move it? Error: ${
@@ -33,8 +32,8 @@ export async function setupE2eApp(
   }
 
   // copy fixtures folder
-  await rm(target, { recursive: true, force: true });
-  await cp(fixture, target, {
+  await fs.rm(target, { recursive: true, force: true });
+  await fs.cp(fixture, target, {
     recursive: true,
     force: true,
     filter(source: string, _: string): boolean | Promise<boolean> {
@@ -44,17 +43,17 @@ export async function setupE2eApp(
 
   // adjust package.json#nx to new location and rename project
   const packageJson = (
-    await readFile(join(target, 'package.json'), 'utf-8')
+    await fs.readFile(path.join(target, 'package.json'), 'utf-8')
   ).toString();
-  await writeFile(
-    join(target, 'package.json'),
+  await fs.writeFile(
+    path.join(target, 'package.json'),
     packageJson
       .replaceAll('fixtures', '__test__')
       .replaceAll(fixtureProjectName, targetProjectName)
   );
   // add e2e fixtures
   if (e2eFixtures) {
-    await cp(e2eFixtures, target, {
+    await fs.cp(e2eFixtures, target, {
       recursive: true,
       force: true,
     });
