a7a3cba52f35e241cc0fa34e1689277007a55520
Michael <michael.hladky@push-based.io>
2025-02-27 20:34:45 +0100

    ci: configure code-pushup js-packages plugin

--- packages/nx-plugin/code-pushup.config.ts
+++ packages/nx-plugin/code-pushup.config.ts
@@ -2,7 +2,7 @@ import { CoreConfig } from '@code-pushup/models';
 import { mergeConfigs } from '@code-pushup/utils';
 import {
   baseConfig,
-  eslintConfig,
+  eslintConfig, jsPackagesConfig
 } from '../../tools/reports/code-pushup.preset.config';
 
-export default mergeConfigs(baseConfig as CoreConfig, await eslintConfig());
+export default mergeConfigs(baseConfig as CoreConfig, await eslintConfig(), await jsPackagesConfig());
