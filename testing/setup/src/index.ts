9e1cf7fdf6b80861ff121a807b1fbdc6d9ec6845
Michael <michael.hladky@push-based.io>
2025-02-28 01:43:28 +0100

    test: add setup lib

---
+++ testing/setup/src/index.ts
@@ -0,0 +1,10 @@
+export const EXCLUDED_FILES_TEST = [
+  'mocks/**',
+  '**/types.ts',
+  '**/*.d.ts',
+  '__snapshots__/**',
+  '**/__tests__/**',
+  '**/code-pushup.config.ts',
+  '**/eslint*.config.js',
+  '**/vitest*.config.mts',
+];
