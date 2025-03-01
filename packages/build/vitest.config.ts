1240d3e8359dad1f53096731eadef77d3ec98943
Michael <michael.hladky@push-based.io>
2025-03-01 11:28:27 +0100

    fix: wip import test setup lib

--- packages/build/vitest.config.ts
+++ packages/build/vitest.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vitest/config';
-import { EXCLUDED_FILES_TEST } from '../../testing/setup/src/index.ts';
+import { EXCLUDED_FILES_TEST } from '@ng-rspack/testing-setup';
 
 export default defineConfig({
   cacheDir: '../../node_modules/.vite/build/unit',
