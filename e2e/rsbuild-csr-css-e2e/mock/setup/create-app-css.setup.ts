0b6a3250732470e4b639da2099c07a6fd84114b8
Michael <michael.hladky@push-based.io>
2025-03-05 01:17:49 +0100

    fix: add project.json

--- e2e/rsbuild-csr-css-e2e/mock/setup/create-app-css.setup.ts
+++
@@ -1,8 +0,0 @@
-import { setupE2eApp } from '../utils';
-import { join } from 'node:path';
-const path = require('path');
-
-export default async function globalSetup() {
-  const fixtureProjectName = 'rsbuild-csr-css';
-  await setupE2eApp(fixtureProjectName, join(__dirname, '../fixtures'));
-}
--- e2e/rsbuild-csr-css-e2e/mock/setup/setup.bin.ts
+++
@@ -1,5 +0,0 @@
-import globalSetup from './create-app-css.setup';
-
-(async () => {
-  await globalSetup();
-})();
